a457f39c451e991a76c175515aace4d8997ed725
Volkov Nikolai <volkov@Volkov.local>
2021-09-09 15:35:15 +0300

    black pre-commit

--- diplom/forms.py
+++ diplom/forms.py
@@ -1,15 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from diplom.models import User, Post
+
 
 class RegistrationForm(FlaskForm):
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
-    email = StringField(
-        "Email", validators=[DataRequired(), Email()]
-    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
 
     confirm_password = PasswordField(
@@ -17,10 +16,14 @@ class RegistrationForm(FlaskForm):
     )
     submit = SubmitField("Sign up")
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is alredy taken.")
+
+
 class LoginForm(FlaskForm):
-    email = StringField(
-        "Email", validators=[DataRequired(), Email()]
-    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me ")
     submit = SubmitField("Log in")
--- diplom/routes.py
+++ diplom/routes.py
@@ -1,47 +1,52 @@
 from flask import render_template, url_for, flash, redirect
 from diplom import app, db, bcrypt
-from diplom.forms import RegistrationForm, LoginForm 
+from diplom.forms import RegistrationForm, LoginForm
 from diplom.models import User, Post
- 
+
 posts = [
     {
         "author": "Kolya Volkov",
         "title": "Blog post 1",
         "content": "First post ever",
-        "date": "September 6, 2021 "
+        "date": "September 6, 2021 ",
     },
     {
         "author": "Dick Tracy",
         "title": "Blog post 2",
         "content": "Second post ever",
-        "date": "September 7, 2021 "
-    }
+        "date": "September 7, 2021 ",
+    },
 ]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts=posts )
+    return render_template("home.html", posts=posts)
+
 
 @app.route("/help")
 def help():
     return render_template("help.html", title="help")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hash_pass = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
-        user = User(username=form.username.data, email=form.email.data, password=hash_pass)
+        user = User(
+            username=form.username.data, email=form.email.data, password=hash_pass
+        )
         db.session.add(user)
         db.session.commit()
         flash(f"Account created for {form.username.data}!", "success")
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    #if form.validate_on_submit():
+    # if form.validate_on_submit():
     return render_template("login.html", title="Login", form=form)
